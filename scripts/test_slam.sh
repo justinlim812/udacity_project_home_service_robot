cb8e40729df74f3c8455ad933c16e88590f430a5
justin <zlim029@e.ntu.edu.sg>
2022-02-09 06:08:10 -0800

    feat: Add a script to test slam function

---
+++ scripts/test_slam.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+terminator  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
+sleep 5
+terminator  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+terminator  -e  " rosrun gmapping slam_gmapping" &
+sleep 5
+terminator  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+terminator  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch"
