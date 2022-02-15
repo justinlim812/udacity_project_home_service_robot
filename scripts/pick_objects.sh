383e37f84f6e6b399ea3e5030f54c2560361cf1d
justin <zlim029@e.ntu.edu.sg>
2022-02-15 06:05:06 -0800

    feat: Change operating environment

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 terminator  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
 sleep 5
-terminator  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &	
+terminator  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/catkin_ws/src/udacity_project_home_service_robot/maps/zheshien.world" &	
 sleep 5
-terminator  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" &
+terminator  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/robond/catkin_ws/src/udacity_project_home_service_robot/maps/zheshien_world.yaml" &
 sleep 5
 terminator  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
