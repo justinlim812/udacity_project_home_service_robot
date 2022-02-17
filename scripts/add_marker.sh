b517bc0e3b87a4524ee054eb36a12f48638453ac
justin <zlim029@e.ntu.edu.sg>
2022-02-17 07:06:42 -0800

    feat: Add test marker code to test marker behaviors

--- scripts/add_marker.sh
+++ scripts/add_marker.sh
@@ -5,6 +5,6 @@ terminator  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=
 sleep 5
 terminator  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/robond/catkin_ws/src/udacity_project_home_service_robot/maps/zheshien_world.yaml" &
 sleep 5
-terminator  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+terminator  -e  " roslaunch turtlebot_rviz_launchers view_navigation_with_marker.launch" &
 sleep 5
-terminator  -e  " rosrun add_markers add_markers" &
+terminator  -e  " rosrun add_markers add_markers_test"
