ffde12d6577d83056a7c227af15f33760dc307d8
justin <zlim029@e.ntu.edu.sg>
2022-02-18 22:18:36 -0800

    fix: Use roslaunch for launch files

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -3,7 +3,7 @@ terminator  -e  " source /opt/ros/kinetic/setup.bash; roscore" &
 sleep 5
 terminator  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find turtlebot_gazebo)/worlds/zheshien.world" &
 sleep 5
-terminator  -e  " rosrun turtlebot_gazebo gmapping_demo.launch" &
+terminator  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
 terminator  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
